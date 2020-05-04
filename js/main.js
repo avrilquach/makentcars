49a391ce0cc24ad60d2d77ca2c973fb7293ef157
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-05-04 10:46:19 -0600

    addfile

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-	$(".js-show-how-it-works").click(function(e){
+	$(".js-show-how-it-works").click(function (e) {
 		$(".how-it-works").show();
 	})
-	$(".js-close-how-it-works").click(function(e){
+	$(".js-close-how-it-works").click(function (e) {
 		$(".js-how-it-works").hide();
 	})
 	$(".host_banner_slides").responsiveSlides();
@@ -19,4 +19,12 @@ $(document).ready(function () {
 			$('.events').append("<li>after event fired.</li>");
 		}
 	});
+	$('.class_date1').click(function(e){
+		$(".class_date1").hide();
+		$('input[name="pickup"]').daterangepicker();
+	})
+	$('.class_date2').click(function(e){
+		$(".class_date2").hide();
+		$('input[name="return"]').daterangepicker();
+	})
 });
