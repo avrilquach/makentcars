4c1afe8dcacf7f58fce5231c5fa9f03b8070138b
family.happy89@gmail.com <flaxnguyen@gmail.com>
2020-05-04 04:10:19 -0600

    first commit

--- js/main.js
+++ js/main.js
@@ -11,6 +11,18 @@ $(document).ready(function () {
 		nav: true,
 		navText: ['<i class="icon icon-chevron-right custom-rotate" aria-hidden="true"></i>', '<i class="icon icon-chevron-right" aria-hidden="true"></i>'],
 		dots: true,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 3
+			},
+			1200: {
+				items: 5
+			}
+		}
 	});
 	$('.experience-slider').owlCarousel({
 		items: 5,
@@ -19,14 +31,38 @@ $(document).ready(function () {
 		nav: true,
 		navText: ['<i class="icon icon-chevron-right custom-rotate" aria-hidden="true"></i>', '<i class="icon icon-chevron-right" aria-hidden="true"></i>'],
 		dots: true,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 3
+			},
+			1200: {
+				items: 5
+			}
+		}
 	});
 	$('#community-slider').owlCarousel({
-		items: 3,
+		items: 5,
 		lazyLoad: true,
 		margin: 20,
 		nav: true,
 		navText: ['<i class="icon icon-chevron-right custom-rotate" aria-hidden="true"></i>', '<i class="icon icon-chevron-right" aria-hidden="true"></i>'],
 		dots: true,
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			768: {
+				items: 3
+			},
+			1200: {
+				items: 5
+			}
+		}
 	});
 	$('input[name="daterange"]').daterangepicker();
 	
